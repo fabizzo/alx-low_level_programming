9728fe3f29818998e70279734fc399b762afde5f
fabizzo <fmaianhi@gmail.com>
2023-02-16 16:26:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,5 +4,8 @@
  *Programming is like building a multilingual puzzle
  *followed by a new line.
  */
-
-int main(void) { puts("\"Programming is like building a multilingual puzzle\n"); return (0); }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
