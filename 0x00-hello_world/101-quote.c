0b8108c1fedba3eb388c380de9d270ed18a82793
fabizzo <fmaianhi@gmail.com>
2023-02-16 18:26:37 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  main - a c progra that prints a line to the standard error
+ *  return 1 (Success)
+ */
+intmain (void)
+{
+	fput ("and that piece of art is uuseful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
