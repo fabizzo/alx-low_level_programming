eee3977dba2bbac4b7d5fe99548db9232340a213
fabizzo <fmaianhi@gmail.com>
2023-02-16 17:00:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main - A C program that prints exactly
- * Programming is like building a multilingual puzzle
- * followed by a new line.
- * Return: 0 (success)
+/**
+ *main - A C program that prints exactly
+ *Programming is like building a multilingual puzzle
+ *followed by a new line.
+ *Return: 0 (success)
  */
 int main(void)
 {
