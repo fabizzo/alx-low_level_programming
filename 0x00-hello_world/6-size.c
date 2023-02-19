96884fe9c38ce0c76f83ad34659bf2c61a8f2683
fabizzo <fmaianhi@gmail.com>
2023-02-19 05:14:10 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
  
 int main(void)
 {
 
-	int a; 
-	long int b; 
-	long long int c; 
-	char d; 
+	int a;
+	long int b;
+	long long int c;
+	char d;
 	float f;
 
 	printf("Size of a char: %lu bytes\n", (char type)sizeof(d));
