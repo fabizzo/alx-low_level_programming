79f4da787d27f9e2c474a59b4c37900ed2b112e6
fabizzo <fmaianhi@gmail.com>
2023-02-22 08:40:00 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
 
+void print_alphabet(void)
 {
-char c;
+	char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-
-_putchar('\n');
-}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
