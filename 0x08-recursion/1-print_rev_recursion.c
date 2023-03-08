cead398c66e7a1d8244363437ac78be4b7e365a0
fabizzo <fmaianhi@gmail.com>
2023-03-08 11:50:02 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints string in reverse
+ * @s: String to be Printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		-putchar(*s);
+	}
+}
