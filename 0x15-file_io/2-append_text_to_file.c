4f8aa07fc18bb70eb44369936b280385dc92cb2e
‘fabizzo’ <fmaianhi@gmail.com>
2023-07-14 10:31:48 -0700

    final file I/O 02

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,10 +28,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-		return (-1); 
+		return (-1);
 
-	close(o); 
+	close(o);
 
-	return (1); 
+	return (1);
 }
 
